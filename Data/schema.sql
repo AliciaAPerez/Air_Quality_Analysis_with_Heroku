87bd6a1b6db94d237544fb135c43273e4eec6f9e
Melanie Nolker <melanie.nolker@kiewit.com>
2021-03-13 10:56:55 -0600

    database load & schema update

--- Data/schema.sql
+++ Data/schema.sql
@@ -2,6 +2,9 @@
 DROP TABLE "AirQuality";
 DROP TABLE "CensusPopulation";
 DROP TABLE "Sites";
+DROP TABLE "DateYear";
+DROP TABLE "year";
+DROP TABLE "Defining_Parameter";
 DROP TABLE "County";
 
 -- AirQuality_DB
@@ -18,15 +21,37 @@ CREATE TABLE "Sites" (
     "site_no" varchar   NOT NULL,
     "Latitude" float   NOT NULL,
     "Longitude" float   NOT NULL,
-    "Elevation" float   		,
-    "Land_Use" varchar          ,
-    "Location_Setting" varchar  ,
+    "Elevation" float   , -- remove NOT NULL
+    "Land_Use" varchar   , -- remove NOT NULL
+    "Location_Setting" varchar   , -- remove NOT NULL
     "State_Name" varchar   NOT NULL,
     "County_Code" int   NOT NULL,
     "City_Name" varchar   NOT NULL,
-    "CBSA_Name" varchar           ,
+    "CBSA_Name" varchar   , -- remove NOT NULL
     CONSTRAINT "pk_Sites" PRIMARY KEY (
         "site_no"
+     )
+);
+
+CREATE TABLE "Defining_Parameter" (
+    "Defining_Parameter" varchar   NOT NULL,
+    CONSTRAINT "pk_Defining_Parameter" PRIMARY KEY (
+        "Defining_Parameter"
+     )
+);
+
+CREATE TABLE "year" (
+    "year" int   NOT NULL,
+    CONSTRAINT "pk_year" PRIMARY KEY (
+        "year"
+     )
+);
+
+CREATE TABLE "DateYear" (
+    "Date" date   NOT NULL,
+    "year" int   NOT NULL,
+    CONSTRAINT "pk_DateYear" PRIMARY KEY (
+        "Date"
      )
 );
 
@@ -37,8 +62,8 @@ CREATE TABLE "AirQuality" (
     "Date" date   NOT NULL,
     "AQI" int   NOT NULL,
     "Category" varchar   NOT NULL,
-    "Defining_Parameter" varchar   NOT NULL,
     "site_no" varchar   NOT NULL,
+    "Defining_Parameter" varchar   NOT NULL,
     CONSTRAINT "pk_AirQuality" PRIMARY KEY (
         "aq_unique_no"
      )
@@ -57,11 +82,23 @@ CREATE TABLE "CensusPopulation" (
 ALTER TABLE "Sites" ADD CONSTRAINT "fk_Sites_County_Code" FOREIGN KEY("County_Code")
 REFERENCES "County" ("county_code");
 
+ALTER TABLE "DateYear" ADD CONSTRAINT "fk_DateYear_year" FOREIGN KEY("year")
+REFERENCES "year" ("year");
+
 ALTER TABLE "AirQuality" ADD CONSTRAINT "fk_AirQuality_county_code" FOREIGN KEY("county_code")
 REFERENCES "County" ("county_code");
+
+ALTER TABLE "AirQuality" ADD CONSTRAINT "fk_AirQuality_Date" FOREIGN KEY("Date")
+REFERENCES "DateYear" ("Date");
 
 ALTER TABLE "AirQuality" ADD CONSTRAINT "fk_AirQuality_site_no" FOREIGN KEY("site_no")
 REFERENCES "Sites" ("site_no");
 
+ALTER TABLE "AirQuality" ADD CONSTRAINT "fk_AirQuality_Defining_Parameter" FOREIGN KEY("Defining_Parameter")
+REFERENCES "Defining_Parameter" ("Defining_Parameter");
+
 ALTER TABLE "CensusPopulation" ADD CONSTRAINT "fk_CensusPopulation_county_code" FOREIGN KEY("county_code")
 REFERENCES "County" ("county_code");
+
+ALTER TABLE "CensusPopulation" ADD CONSTRAINT "fk_CensusPopulation_year" FOREIGN KEY("year")
+REFERENCES "year" ("year");
