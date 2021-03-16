e2707e861ce4307d88e1181e707b0c5040058b86
Julie Headlee <julieheadlee@gmail.com>
2021-03-16 12:52:38 -0500

    Produced yearly data table

--- static/js/yearlyvpop.js
+++ static/js/yearlyvpop.js
@@ -1,60 +1,27 @@
-// let select = d3.select("select");
-// //import data
+var tbody = d3.select("tbody");
 
-// //insert data into HTML for view upon opening page
-// data.forEach(({county}) =>{
-//     let option = select.append(`option value= ${value}`).text(county);
-// });
-
-// option = select.append(`option value= ${value}`).text(county);
-// select.append(`option value= ${value}`).text(county);
 function getData(selOption) {
-    console.log("I am in getData()")
+    console.log("I am in new getData()")
     var value = selOption;  
     console.log("My value is: " + value);
 
+    d3.csv("getPlotCSV").then(function(saqiData) {
+
+        const data = saqiData;
+        console.log(data[0]);
+        // Filter the data to only those equaling the input field value
+        var filteredData = data.filter(aqiData => aqiData.Date.substring(0,4) == value);
+        console.log(filteredData);
+        // Need to empty the table data before appending those meeting criteria
+        tbody = d3.select("tbody")
+        tbody.html("")
+
+        filteredData.forEach((AQIRecord) => {
+            var row = tbody.append("tr");
+            Object.entries(AQIRecord).forEach(([key, value]) => {
+                var cell = row.append("td");
+                cell.text(value);
+            })
+        })
+    })
 }
-// $("#selDataset").on("change", function runFilter() {
-//     d3.event.preventDefault();
-
-
-//     // d3.csv("getPlotCSV").then(function(saqiData) {
-//     //     console.log(saqiData)
-//     // });
-
-//     var yearValue = []
-
-//     d3.csv("getPlotCSV").then(function(saqiData) {
-
-
-//         const data = saqiData;
-//         console.log(data)
-
-//         for (let [value] of Object.entries(data)) {
-//             for (let index = 0; index < data.length; index++) {
-//                 const aqiData = data[index];
-
-//                 var yearValue = aqiData.year.append()
-            
-//                 var yearValue = document.getElementById("year").value;
-//             // var cityValue = document.getElementById("city").value.toLowerCase();
-//             // var stateValue = document.getElementById("state").value.toLowerCase();
-//             // var countryValue = document.getElementById("country").value.toLowerCase();
-//             // var shapeValue = document.getElementById("shape").value.toLowerCase();
-
-            
-            
-//             var filteredData = tableData;
-//         //Fitler each search criteria
-//             if (dateValue !== ""){
-//                 filteredData = filteredData.filter(data => data.datetime === dateValue);
-//             }
-
-//     // Get a reference to the table body
-//             var tbody = d3.select("tbody");
-//             tbody.html("");
-//             }
-//         }
-//     });
-// });
-
